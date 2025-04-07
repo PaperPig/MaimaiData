2adaa38bdd26e54e3d14b156092633c1673981d2
PaperPig <PaperPig@vip.qq.com>
2025-04-07 16:03:10 +0800

    fix:修改最大缩放倍数

--- app/src/main/java/com/paperpig/maimaidata/widgets/PinchImageView.java
+++ app/src/main/java/com/paperpig/maimaidata/widgets/PinchImageView.java
@@ -38,7 +38,7 @@ public class PinchImageView extends androidx.appcompat.widget.AppCompatImageView
     /**
      * 图片最大放大比例
      */
-    private static final float MAX_SCALE = 4f;
+    private static final float MAX_SCALE = 2f;
 
 
     ////////////////////////////////监听器////////////////////////////////
